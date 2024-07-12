85252e3112463cf7606cc93afef45df58e7dfd8d
Gustavo Silva <gustavomachado.gms@hotmail.com>
2024-07-11 22:49:54 -0300

    organizando trabalho e testando criar libs

---
+++ view/telaInicial.c
@@ -0,0 +1,8 @@
+//
+//  telaInicial.c
+//  INFmon
+//
+//  Created by Gustavo Machado Silva on 11/07/24.
+//
+
+#include "telaInicial.h"
---
+++ view/telaInicial.h
@@ -0,0 +1,13 @@
+//
+//  telaInicial.h
+//  INFmon
+//
+//  Created by Gustavo Machado Silva on 11/07/24.
+//
+
+#ifndef telaInicial_h
+#define telaInicial_h
+
+#include <stdio.h>
+
+#endif /* telaInicial_h */
