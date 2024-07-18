ad4b0ff2739cce9ec9fd99cac7259ec2e8075571
Gustavo Silva <gustavomachado.gms@hotmail.com>
2024-07-17 22:29:56 -0300

    testando exibição do mapa

---
+++ view/exibeMapa.c
@@ -0,0 +1,8 @@
+//
+//  exibeMapa.c
+//  INFmon
+//
+//  Created by Gustavo Machado Silva on 17/07/24.
+//
+
+#include "exibeMapa.h"
---
+++ view/exibeMapa.h
@@ -0,0 +1,13 @@
+//
+//  exibeMapa.h
+//  INFmon
+//
+//  Created by Gustavo Machado Silva on 17/07/24.
+//
+
+#ifndef exibeMapa_h
+#define exibeMapa_h
+
+#include <stdio.h>
+
+#endif /* exibeMapa_h */
