acc19c2254e8873590e6dc23fc87d0642ec6a1ac
Gustavo Silva <gustavomachado.gms@hotmail.com>
2024-07-22 07:13:28 -0300

    Organizando e separando o que já está pronto

---
+++ control/controleTelaInicial.c
@@ -0,0 +1,8 @@
+//
+//  controleTelaInicial.c
+//  INFmon
+//
+//  Created by Gustavo Machado Silva on 21/07/24.
+//
+
+#include "controleTelaInicial.h"
---
+++ control/controleTelaInicial.h
@@ -0,0 +1,13 @@
+//
+//  controleTelaInicial.h
+//  INFmon
+//
+//  Created by Gustavo Machado Silva on 21/07/24.
+//
+
+#ifndef controleTelaInicial_h
+#define controleTelaInicial_h
+
+#include <stdio.h>
+
+#endif /* controleTelaInicial_h */
